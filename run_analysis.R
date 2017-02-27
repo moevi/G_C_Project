69eda29e24b6479e485e7f39af925e6f05d7371d
moevi <kmoevi@yahoo.fr>
2017-02-27 00:57:18 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,20 +4,19 @@ download.file( project_url,destfile = "./project_dir/project_zip_file.zip",metho
 unzip(zipfile="./project_dir/project_zip_file.zip",exdir="./project_dir")
 list.files("project_dir",recursive = TRUE)
 
---train set
+
 
 subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE )
 x_train<-read.table("./project_dir/UCI HAR Dataset/train/x_train.txt",header=FALSE )
 y_train<-read.table("./project_dir/UCI HAR Dataset/train/y_train.txt",header=FALSE )
 
---test set
 
 
 subject_test<-read.table("./project_dir/UCI HAR Dataset/test/subject_test.txt",header=FALSE )
 x_test<-read.table("./project_dir/UCI HAR Dataset/test/x_test.txt",header=FALSE )
 y_test<-read.table("./project_dir/UCI HAR Dataset/test/y_test.txt",header=FALSE )
 
---Mergin Data
+#Mergin Data
 x_set<-rbind(x_train,x_test)
 y_set<-rbind(y_train,y_test)
 subject_set<-rbind(subject_train,subject_test)
@@ -34,3 +33,28 @@ dim(mean_std)
  y_set[, 1]<-read.table("./project_dir/UCI HAR Dataset/activity_labels.txt")[y_set[,1],2]
 names(y_set) <- "Activity"
  View(activity)
+
+names(subject_set)<-"Subject"
+
+ s_set<-cbind(mean_std,y_set,subject_set)
+colnames<-colnames(s_set)
+colnames<-make.names(colnames,unique=TRUE)
+colnamesclean<-gsub("-", " ", colnames)
+colnamesclean<-gsub("\\.", " ", colnamesclean)
+colnamesclean<-gsub("\\  ", " ", colnamesclean)
+colnamesclean<-gsub("\\  ", " ", colnamesclean)
+colnamesclean<-gsub("\\  ", " ", colnamesclean)
+colnamesclean<-gsub("tBody", "Body", colnamesclean)
+colnamesclean<-gsub("tGravity", "Gravity", colnamesclean) 
+colnamesclean<-gsub("fBody", "Body", colnamesclean)
+colnamesclean<-gsub("BodyBody", "Body", colnamesclean) 
+colnamesclean<-gsub("^\\s+|\\s+$", "", colnamesclean)
+colnames(s_set)
+
+
+## creates a second, independent tidy ,
+
+ind_data<-aggregate(. ~Subject + Activity, s_set, mean)
+int_data<-int_data[order(int_data$Subject,int_data$Activity),]
+write.table(int_data, file = "tidydata.txt",row.name = FALSE)
+
