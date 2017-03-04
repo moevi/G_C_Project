d74ed65868a1483530d7524b7be8cd6046d9b7e6
moevi <kmoevi@yahoo.fr>
2017-03-04 14:23:49 -0600

    Update the CodeBook and run_analysis.R

--- CodeBook.md
+++ CodeBook.md
@@ -1,1 +1,31 @@
-mm
+ CODEBOOK - GETTING AND CLEANING DATA PROJECT
+DESCRIPTION
+* The purpose of this project is to demonstrate the ability to collect, work with, and clean a data set.
+PROJECT GOAL
+* The goal is to prepare tidy data that can be used for later analysis.
+PROJECT OBJECTIVES
+* Produce a tidy dataset
+* A link to a Github repository with script for performing the analysis
+* A code book (CodeBook.md) that
+o Describes the variables
+o The data
+o Any transformations or work that was performed to clean up the data
+* A README.md file that explains how all of the scripts work and how they are connected.
+
+DEPENDENCIES
+* DATA UCI HAR Dataset (Human Activity Recognition Using Smartphones Data Set Version 1.0)
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+1.The run_analysis script downloads  and   loads the data into R 
+the following variables are used to create the test, train and subject data in R.
+x_test
+y_test 
+subject_test
+subject_set
+2.we  extract  the mean  and standard deviation after loading  the activity_label.txt  and features.txt data into R.
+The mean_std variable store the extracted mean and standard deviation.
+3.In the activity data frame 
+               4.I labeled  the measurement column with the appropriate name  
+ 
+
+              5 I created a new tidy data
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+##Download and import data in R
 project_url<-'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 if(!file.exists("./project_dir")){dir.create("./project_dir")}
 download.file( project_url,destfile = "./project_dir/project_zip_file.zip",method= 'curl')
@@ -6,7 +7,7 @@ list.files("project_dir",recursive = TRUE)
 
 
 
-subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE )
+subject_train<-read.table("./project_dir/UCI HAR Dataset/train/subject_train.txt",header=FALSE )
 x_train<-read.table("./project_dir/UCI HAR Dataset/train/x_train.txt",header=FALSE )
 y_train<-read.table("./project_dir/UCI HAR Dataset/train/y_train.txt",header=FALSE )
 
@@ -16,7 +17,7 @@ subject_test<-read.table("./project_dir/UCI HAR Dataset/test/subject_test.txt",h
 x_test<-read.table("./project_dir/UCI HAR Dataset/test/x_test.txt",header=FALSE )
 y_test<-read.table("./project_dir/UCI HAR Dataset/test/y_test.txt",header=FALSE )
 
-#Mergin Data
+#Question 1 Mergin Data
 x_set<-rbind(x_train,x_test)
 y_set<-rbind(y_train,y_test)
 subject_set<-rbind(subject_train,subject_test)
@@ -25,18 +26,18 @@ dim(subject_set)
 dim(x_set)
 dim(y_set)
 
-
+##Mean 
 mean_std <- x_set[, grep("-(mean|std)\\(\\)", read.table("./project_dir/UCI HAR Dataset/features.txt")[, 2])]
 
 dim(mean_std)
 
- y_set[, 1]<-read.table("./project_dir/UCI HAR Dataset/activity_labels.txt")[y_set[,1],2]
+y_set[, 1]<-read.table("./project_dir/UCI HAR Dataset/activity_labels.txt")[y_set[,1],2]
 names(y_set) <- "Activity"
- View(activity)
+View(activity)
 
 names(subject_set)<-"Subject"
 
- s_set<-cbind(mean_std,y_set,subject_set)
+s_set<-cbind(mean_std,y_set,subject_set)
 colnames<-colnames(s_set)
 colnames<-make.names(colnames,unique=TRUE)
 colnamesclean<-gsub("-", " ", colnames)
